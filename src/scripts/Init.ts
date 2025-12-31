dab26bc30ab0a66d1a79a465501ff1b65cf26542
yanzaiyun43 <132528785+yanzaiyun43@users.noreply.github.com>
2025-12-31 21:46:47 +0800

    Update Init.ts

--- src/scripts/Init.ts
+++ src/scripts/Init.ts
@@ -108,6 +108,5 @@ export default () => {
     MusicList.forEach((i: any) => i.destroy());
     MusicList.length = 0;
   });
-  console.log("%c🌻 程序：Astro | 主题：vhAstro-Theme | 作者：Han | Github：https://github.com/uxiaohan/vhAstro-Theme 🌻", "color:#fff; background: linear-gradient(270deg, #18d7d3, #68b7dd, #8695e6, #986fee); padding: 8px 15px; border-radius: 8px");
   console.log("%c\u521D\u59CB\u5316\u5B8C\u6BD5.", "color: #ffffff; background: #000; padding:5px");
 }
