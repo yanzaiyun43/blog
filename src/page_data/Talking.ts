7285faf5ef08326cfc4026f98a03be11bd795ca0
yanzaiyun43 <132528785+yanzaiyun43@users.noreply.github.com>
2026-01-01 20:43:01 +0800

    Update Talking.ts

--- src/page_data/Talking.ts
+++ src/page_data/Talking.ts
@@ -1,6 +1,6 @@
 export default {
   // API 接口请求优先，数据格式保持和 data 一致
-  api: 'http://ailmel.ct.ws/talking.php',
+  api: 'https://ailmel.ct.ws/talking.php',
   // api 为空则使用 data 静态数据 
   // 注意：图片请用 vh-img-flex 类包裹
   data: [
