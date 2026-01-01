25aa2b1bf7347d7102d6c649bdbb06f527ba9828
yanzaiyun43 <132528785+yanzaiyun43@users.noreply.github.com>
2026-01-01 21:31:48 +0800

    Update Talking.ts

--- src/page_data/Talking.ts
+++ src/page_data/Talking.ts
@@ -1,6 +1,6 @@
 export default {
   // API 接口请求优先，数据格式保持和 data 一致
-  api: 'https://ailmel.dpdns.org/API/talking.php',
+  api: 'https://ailmel.dpdns.org/API/talking.php?apikey=23f2e4dfcd2e11704b7a29973b8a347765547a32678382f1800dcd49dd3e8bcd',
   // api 为空则使用 data 静态数据 
   // 注意：图片请用 vh-img-flex 类包裹
   data: [
