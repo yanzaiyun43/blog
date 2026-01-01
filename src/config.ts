a3c93f3bd1609b177974f41d9eb0e5ba14e479d1
yanzaiyun43 <132528785+yanzaiyun43@users.noreply.github.com>
2026-01-01 10:45:17 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -65,7 +65,7 @@ export default {
     // 仅支持 SVG 且 SVG 需放在 public/assets/images/svg/ 目录下，填入文件名即可 <不需要文件后缀名>（封装了 SVG 组件 为了极致压缩 SVG）
     // 建议使用 https://tabler.io/icons 直接下载 SVG
     { text: 'Github', link: 'https://github.com/yanzaiyun43', icon: 'WebSite_github' },
-    { text: 'API', link: 'https://api.vvhan.com', icon: 'WebSite_api' },
+    { text: 'API', link: 'https://ailmel.dpdns.org/', icon: 'WebSite_api' },
 //    { text: '每日热榜', link: 'https://hot.vvhan.com', icon: 'WebSite_hot' },
     { text: '海阔图床', link: 'https://img.167891.xyz/', icon: 'WebSite_img' },
 //    { text: 'HanAnalytics', link: 'https://analytics.vvhan.com', icon: 'WebSite_analytics' },
