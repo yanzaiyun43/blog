8f52cd480386c89416d2a5a111bdd65b4b17d2ba
yanzaiyun43 <132528785+yanzaiyun43@users.noreply.github.com>
2026-01-06 16:58:24 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -10,7 +10,7 @@ export default {
   // 网站作者
   Author: '𝓪𝓲𝓵𝓶𝓮𝓵',
   // 作者头像
-  Avatar: 'https://p.cldisk.com/star4/c648fc285283b68e6e3a2e310b47b7cf/origin.jpg?rw=778&rh=778&_fileSize=563200&_orientation=1',
+  Avatar: 'https://img.167891.xyz/v2/3VHU8UR.png',
   // 网站座右铭
   Motto: 'Cogito ergo sum.',
   // Cover 网站缩略图
