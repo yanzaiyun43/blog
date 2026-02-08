26e6e2ea8b9dffff5ab3d2955ca4864902be25ab
yanzaiyun43 <132528785+yanzaiyun43@users.noreply.github.com>
2026-02-08 23:07:06 +0800

    Update Link.ts

--- src/page_data/Link.ts
+++ src/page_data/Link.ts
@@ -1,16 +1,61 @@
 export default {
   // API 接口请求优先，数据格式保持和 data 一致
-  api: 'https://ailmel.dpdns.org/API/link.php',
+  api: '',
   // api 为空则使用 data 静态数据
-  data: [
+  data:[
     {
-      "name": "海阔图床",
-      "link": "https://img.167891.xyz/",
-      "avatar": "https://img.167891.xyz/assets/logo-3FdGxyKZ.webp",
-      "descr": "我思故我在."
+        "name": "安和鱼",
+        "link": "https:\/\/blog.anheyu.com\/",
+        "avatar": "https:\/\/npm.elemecdn.com\/anzhiyu-blog-static@1.0.4\/img\/avatar.jpg",
+        "descr": "生活明朗，万物可爱"
     },
-//    {
- //
- //   }
-  ]
+    {
+        "name": "绘星里",
+        "link": "https:\/\/blog.storia.ren\/",
+        "avatar": "https:\/\/blog.storia.ren\/images\/icon.png",
+        "descr": "一起来绘制属于自己的星星！"
+    },
+    {
+        "name": "自由的博客",
+        "link": "https:\/\/blog.freenw.com",
+        "avatar": "https:\/\/img.fastmirror.net\/s\/2025\/10\/02\/68de75caaf3fc.jpeg",
+        "descr": "初中生技术爱好者的公益分享平台，聚焦技术、学习与成长干货。"
+    },
+    {
+        "name": "龙腾万里sky的博客",
+        "link": "https:\/\/blog.aiwanli.top",
+        "avatar": "https:\/\/blog.aiwanli.top\/images\/avatar.jpg",
+        "descr": "养得胸中一种恬静。"
+    },
+    {
+        "name": "天渺studio",
+        "link": "https:\/\/tianmiao.site",
+        "avatar": "https:\/\/s21.ax1x.com\/2024\/12\/22\/pAXtJat.jpg",
+        "descr": "天渺工作室的小站&日志记录"
+    },
+    {
+        "name": "JETT",
+        "link": "https:\/\/jettlin.cn",
+        "avatar": "https:\/\/api.jettlin.cn\/api\/files\/1757467798643-user-img.png",
+        "descr": "暮然回首，那人却在，灯火阑珊处"
+    },
+    {
+        "name": "星河避难所",
+        "link": "https:\/\/hejunjie.life",
+        "avatar": "https:\/\/cdn.hejunjie.life\/avatar.jpg",
+        "descr": "写代码，也写自己"
+    },
+    {
+        "name": "Levi's space",
+        "link": "https:\/\/leviqin.top",
+        "avatar": "https:\/\/i.postimg.cc\/cJmdWqkn\/Snipaste-2024-04-24-16-54-57.png",
+        "descr": "一个有意思的Blog，工作记录，日常分享，欢迎访问"
+    },
+    {
+        "name": "海阔图床",
+        "link": "https:\/\/img.167891.xyz\/",
+        "avatar": "https:\/\/img.167891.xyz\/v2\/lEEu0hA.png",
+        "descr": "我的图床"
+    }
+]
 }
